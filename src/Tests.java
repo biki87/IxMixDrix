70dca42aabe694cb0f8575112c1f7e28846207dd
biki87 <bikov.dima@gmail.com>
2015-03-23 02:16:58 +0200

    Add all files to commit

---
+++ src/Tests.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Dima on 3/23/2015.
+ */
+public class Tests {
+    public static void main(String[] args){
+        Board board = new Board();
+        board.printBoard();
+    }
+}
